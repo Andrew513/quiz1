b475e30d28555ee3dc73f53a3e7b35ffe8db307f
Your NameAndrew513 <brave.lei40915@gmail.com>
2021-03-29 21:16:39 +0800

    quiz 1-3

--- quiz1.js
+++ quiz1.js
@@ -1,10 +1,14 @@
 function change() {
-    var header = document.getElementById("quiz1");
-    header.innerHTML = "CSIE@CGU";
-    var para = document.getElementById("quiz1_1");
-    para.innerHTML = "怎麼那麼棒！！";
+    $(document).ready(function(){
+        $("#quiz1").text("CSIE@CGU")
+        $("#quiz1_1").text("怎麼那麼棒！！")
+    });
+    // var header = document.getElementById("quiz1");
+    // header.innerHTML = "CSIE@CGU";
+    // var para = document.getElementById("quiz1_1");
+    // para.innerHTML = "怎麼那麼棒！！";
     }
-function myFunction() {  
+function myFunction() {
     var btn = document.createElement("BUTTON");  
     btn.innerHTML = "Change this document";
     document.body.appendChild(btn).addEventListener("click", change);
